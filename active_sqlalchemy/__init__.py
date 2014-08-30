ab77a9af26f3928c906ba51c49ddb9dd90b0833e
Mardix <mcx2082@gmail.com>
2014-08-30 17:27:23 -0400

    Bump version

--- active_sqlalchemy/__init__.py
+++ active_sqlalchemy/__init__.py
@@ -13,7 +13,7 @@ to use by implementing a simple active record like api, while it still uses the 
 """
 
 NAME = "Active-SQLAlchemy"
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = "Mardix"
 __license__ = "MIT"
 __copyright__ = "2014 - Mardix"
