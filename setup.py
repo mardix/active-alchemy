e18925be920815a8b23b2ac0ac46fe6b4c4f5ef3
Mardix <mardix@pylot.io>
2019-11-11 22:00:12 -0500

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ Active-Alchemy
 A framework agnostic wrapper for SQLAlchemy that makes it really easy
 to use by implementing a simple active record like api, while it still uses the db.session underneath
 
-:copyright: © 2014/2017 by `Mardix`.
-:license: MIT, see LICENSE for more details.
+copyright © 2019 forver by `Mardix`.
+license MIT, see LICENSE for more details.
 
 """
 
@@ -16,7 +16,7 @@ from setuptools import setup
 
 NAME = "Active-Alchemy"
 py_module = "active_alchemy"
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = "Mardix"
 __license__ = "MIT"
 __copyright__ = "2014/2017 - Mardix"
@@ -27,7 +27,7 @@ setup(
     license=__license__,
     author=__author__,
     author_email='mardix@pylot.io',
-    description=__doc__,
+    description="A framework agnostic wrapper for SQLAlchemy with a simple active record like api",
     long_description=__doc__,
     url='http://github.com/mardix/active-alchemy/',
     download_url='http://github.com/mardix/active-alchemy/tarball/master',
